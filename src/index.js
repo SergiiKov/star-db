5e575ebc318283c1556498bba62cf2def14037ec
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-05 15:58:20 +0300

    get api from https://swapi.dev/

--- src/index.js
+++ src/index.js
@@ -8,16 +8,38 @@ class  SwapiService {
     }
     return await res.json();
   }
-  getAllPeople() {
-    return this.getResours(`/people/`);
+ async getAllPeople() {
+    const res = await this.getResours(`/people/`);
+    return res.results;
   }
   getPerson(id) {
     return this.getResours(`/people/${id}`);
   }
+
+  async getAllPlanets() {
+    const res = await this.getResours(`/planets/`);
+    return res.results;
+  }
+  getPlanet(id) {
+    return this.getResours(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResours(`/starships/`);
+    return res.results;
+  }
+  getStarship(id) {
+    return this.getResours(`/starships/${id}`);
+  }
+
 }
+
+
 
 const swapi = new SwapiService();
 
-swapi.getAllPeople().then((body)=>{
-  console.log(body)
+swapi.getAllStarships().then((starships)=>{
+  starships.forEach((p)=>{
+    console.log(p.name)
+  })
 })
