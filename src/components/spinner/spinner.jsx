dbcb8924e46f22251f60deedb0724c2c1d1adc60
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-09 16:50:04 +0300

    add spinner to planet loading

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './spinner.style.scss';
+
+export default class ItemList extends Component {
+
+  render() {
+    return (
+        <div class="loadingio-spinner-spinner-s9gbloa56q"><div class="ldio-vnn9n4fs3rs">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    );
+  }
+}
